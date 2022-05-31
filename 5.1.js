8d0c8b033c4476182e0b52ab3ab29d4aab6b1a0c
Sebastien Voos <sebastien.voos@hotmail.com>
2022-05-31 15:57:23 +0200

    presque ok5.2

--- 5.1.js
+++ 5.1.js
@@ -10,19 +10,19 @@ const readlineSync = require("readline-sync");
  * @return {[objet]}[objet serie compose du nom ,annee ,castMembers]
  */
 ////////////////////////////////////
+let castMembers=[];
 
 function askTvSerie() {
-    var castMembers=[];
-    var name= readlineSync.question('name of TV Show : ');
-    var productionYear= readlineSync.question('production year of TV Show : ');
-    let i=0;
-    while(i!="stop"){
-         i= readlineSync.question('cast members of TV Show !TAPEZ stop POUR SORTIR!');
-         if(i!="stop"){
-        castMembers.push(i);
-        console.log(castMembers);
-         }
+  var name= readlineSync.question('name of TV Show : ');
+  var productionYear= readlineSync.question('production year of TV Show : ');
+  let i=0;
+  while(i!="stop"){
+    i= readlineSync.question('cast members of TV Show !TAPEZ stop POUR SORTIR!');
+    if(i!="stop"){
+    castMembers.push(i);
+    console.log(castMembers);
     }
+  }
     var serie = {name, productionYear, castMembers}
     console.log("CECI EST NOTRE OBJET 'SERIE' ");
     console.log(serie);
@@ -37,52 +37,54 @@ askTvSerie();
 ////////////////////////////////////
 
 function randomizeCast(arr){
-        let i = arr.length,  
-        randomIndex=i;
-        while ((i-1) != 0) {
-          randomIndex = Math.floor(Math.random() * i);
-          i--;
-          [arr[i], arr[i]] = [
-            arr[i], arr[i]];
-            console.log(arr);
-        }
-        console.log(arr);
-        
-}
-tvSerie = [3,5,2,5,59,9,9,5];
-randomizeCast(tvSerie);
-/*
-for (i = 0; i < serie.castMembers.length; i++) {
-    numbersCopy[i] = castMembers[i];
+  let i = arr.length;
+  while (i>0) {
+    let randIndex=Math.floor(Math.random()*(i+1));
+    [arr[randIndex], arr[i]] = [arr[i], arr[randIndex]];
+    i--;
   }
-numbersCopy.push(4);
-console.log(castMembers, 
-    numbersCopy);
-/*
-let tvSerie=[];
-askTvSerie();
-tvSerie = serie.castMembers.filter(() => true);
-console.log(tvSerie);
-/*
-COPIER castMembers dans autre array puis travaille avec ce new array
-randomizeCast();
-*/
+}
+let newSerie = askTvSerie();
+randomizeCast(castMembers);
+console.log(`Randomized cast : ${castMembers}`)
+console.log("objet serie une fois modifie");
+console.log(JSON.stringify(newSerie));
+;
 
 //////////////////////////////////////
 ///////////Exercise 5.3//////////////
 ////////////////////////////////////
-
+/*
   function nestedArrayAndObject(){
       const info =['Once Upon a Time','Emma Swan','Regina Mills','Cora Mills','Queen of Hearts','Peter Pan',`The boy who wouldn't grow up`,'Zelena', 'The Wicked Witch'];
-      const[title,info.protagonist.name , info.protagonist.enemies[3] ,enemy.title ,enemy.name]=info;
-      /*
-      const title = info.title
+      //const[title,info.protagonist.name , info.protagonist.enemies[3] ,enemy.title ,enemy.name]=info;
+    const title = info.title
     const protagonistName = info.protagonist.name
     const enemy = info.protagonist.enemies[3]
     const enemyTitle = enemy.title
     const enemyName = enemy.name
-    */
+    
     return `${enemyName} (${enemyTitle}) is an enemy to ${protagonistName} in "${title}"`
   }
-  nestedArrayAndObject();
 
+  nestedArrayAndObject();
+*/
+let test = function nestedArrayAndObject() {
+
+  const info = {
+      title: 'Once Upon a Time', 
+      protagonist: {
+          name: 'Emma Swan', 
+          enemies: [
+              {name: 'Regina Mills', title: 'Evil Queen'},
+              {name: 'Cora Mills', title: 'Queen of Hearts'},
+              {name: 'Zelena', title: 'The Wicked Witch'},
+              {name: 'Peter Pan', title: `The boy who wouldn't grow up`},
+          ],
+      }
+  }
+  let {title: serie , protagonist: {name: Hero, enemies: [{name: Enemies, title: title}]} } = info
+      return `${Enemies} (${title}) is an enemy to ${Hero} in "${serie}`
+  }
+  
+  console.log(test());
