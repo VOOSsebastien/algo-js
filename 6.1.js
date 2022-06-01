b144ae8c0986dba0a651993004b8e18d060faf4a
Sebastien Voos <sebastien.voos@hotmail.com>
2022-06-01 15:48:10 +0200

    ok

--- 6.1.js
+++ 6.1.js
@@ -34,27 +34,29 @@ new Person("VOOS ","Sebastien").sayHello();
 ////////////////////////////////////
 
 
-const Animal = { 
+class Animal  { 
+    constructor(name){
+        this.name=name;
+    }
     sayHello() {
         console.log(  `${this.greeting}! I'm ${this.name}!`);
         }
 }
-class Cat2  {
-    constructor(name, greeting){
-        this.name=name;
-        this.greeting=greeting;
+class Cat2  extends Animal{
+    constructor(name, age){
+        super(name);
+        this.age=age;
     }
+    static greeting="miaou";
+
 }
-class Dog  {
-    constructor(name, greeting){
-        this.name=name;
-        this.greeting=greeting;
-    }
+class Dog extends Animal {
+    static greeting="woufwouf";
 }
-Object.setPrototypeOf(Cat2.prototype, Animal);
-let c = new Cat2('LEA','BONJOUR');
-Object.setPrototypeOf(Dog.prototype, Animal);
-let d = new Dog('AZERTY','SALUT!TOI!');
+//Object.setPrototypeOf(Cat2.prototype, Animal);
+let c = new Cat2("billy",3);
+//Object.setPrototypeOf(Dog.prototype, Animal);
+let d = new Dog('rex');
 c.sayHello();
 d.sayHello();
 
@@ -68,14 +70,14 @@ class Person1 {
       this.firstName = firstName;
       this.lastname = lastname;
     }
-    get name() {
-      return this.firstName+ this.lastname;
+    get getname() {
+      return this.firstName+"/" +this.lastname;
     }
-    set name(val) {
+    set setname(val) {
       console.log("new NAME " +this.firstName + " " + this.lastname);
     }   
   }
   let obj1 = new Person1("Clark", "kent");
-  console.log(obj1.name);  
-  obj1.name = ("clark","kent"); 
+  console.log(obj1.getname);  
+  obj1.setname = ("clark","kent"); 
   
